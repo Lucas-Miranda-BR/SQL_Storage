69ee06d9fe90de86ea6894f4b9a8aa0aacd040c8
Lucas <lucasmirandabr0508@gmail.com>
2025-08-14 14:34:32 -0300

    Atualização de crud.sql
    Remoção de Select, Delete, Update, Insert (Temporamenete)
    Tabelas & Relações atualizadas

--- crud.sql
+++ crud.sql
@@ -26,20 +26,16 @@ id_categoria int,
 id_fornecedor int
 )engine=innodb;
 
-create table fornecedor(
-id int primary key auto_increment
-)engine=innodb;
-
 create table pedido_detalhe(
 id_produto int,
 id_pedido int,
 subtotal decimal(8,2) default 0,
-quantidade int default 0
+quantidade int default 0,
+primary key (id_pedido, id_produto)
 )engine=innodb;
 
 create table marca(
 id int primary key auto_increment,
-id_produto int,
 nome varchar(50) not null,
 contato_nomeVendedor varchar(50) not null,
 contato_telefoneVendedor char(11) unique
@@ -47,7 +43,6 @@ contato_telefoneVendedor char(11) unique
 
 create table categoria(
 id int primary key auto_increment,
-id_produto int,
 descricao varchar(50) not null,
 setor varchar(50) not null
 )engine=innodb;
@@ -59,36 +54,15 @@ nome_fantasia varchar(50) not null,
 cnpj char(14) unique,
 endereco varchar(50) not null,
 bairro varchar(50) not null,
-rua varchar(50) not null,
-cep char(8) not null
+cidade varchar(50) not null,
+estado varchar(50) not null,
+cep char(8) not null,
+telefone char(11) unique
 )engine = innodb;
 
-alter table categoria add constraint fk_categoriaXproduto foreign key categoria(id_produto) references produto(id);
-alter table marca add constraint fk_marcaXproduto foreign key marca(id_produto) references produto(id);
 alter table pedido_detalhe add constraint fk_pedido_detalheXproduto foreign key pedido_detalhe(id_produto) references produto(id);
 alter table pedido_detalhe add constraint fk_pedido_detalheXpedido foreign key pedido_detalhe(id_pedido) references pedido(id);
 alter table pedido add constraint fk_vendedorXpedido foreign key pedido(id_vendedor) references vendedor(id);
-
-insert into produto(descricao, validade, quantidade) values("Mel do Abacaxi", '12-04-2037', 30);
-insert into categoria(id_produto, descricao, setor) values(1, "Cosmestiveis Doces", "Doces / Salgados");
-insert into marca(nome, contato_nomeVendedor, contato_telefoneVendedor) values("Meltz", "Pietro", 0055554444);
-insert into pedido_detalhe values(1, 1, 5.99, 1);
-insert into vendedor values(0, "Pedro", 3500.00, 99999999999, 12345678901);
-insert into vendedor(nome, tel, cpf) values("Pablo", 88888888888, 09876543211);
-insert into vendedor(nome, cpf) values("João", 01928374650);
-insert into pedido(valor_total, id_vendedor) values(35.00, 3);
-insert into pedido(valor_total, id_vendedor) values(71.40, 2);
-
-select * from vendedor;
-select * from pedido;
-select * from marca;
-select * from produto;
-select * from pedido_detalhe;
-select * from fornecedor;
-
-delete from pedido where id = 2;
-delete from produto where id = 2;
-
-update vendedor set salario = 2500.00, tel = 10293847562 where id = 2;
-update vendedor set salario = 1820.00, cpf = 9876543218, tel = 12345678900 where id = 3;
-update marca set id_produto = 1, contato_telefoneVendedor = 9955554444 where id = 1;
+alter table produto add constraint fk_produtoXmarca foreign key produto(id_marca) references marca(id);
+alter table produto add constraint fk_produtoXcategoria foreign key produto(id_categoria) references categoria(id);
+alter table produto add constraint fk_produtoXfornecedor foreign key produto(id_fornecedor) references fornecedor(id);
